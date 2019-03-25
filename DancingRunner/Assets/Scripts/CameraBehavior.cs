3d95329a07cbd2f93c99abb4db0fc8ca1e90c611
Hüsler Etienne <etienne.husler@he-arc.ch>
2019-03-25 11:27:01 +0100

    Camera smooth movement between savepoints

---
+++ DancingRunner/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public GameObject level;
+    public Transform player;
+    public float smoothTime;
+    
+    private List<GameObject> stages;
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stages = new List<GameObject>();
+        foreach (Transform child in level.transform)
+        {
+            if (child.name.Contains("Stage"))
+            {
+                stages.Add(child.gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int cameraIndex = GetCameraIndex();
+        if (cameraIndex != -1)
+        {
+            PlaceCamera(stages[cameraIndex].transform.Find("Start"), stages[cameraIndex + 1].transform.Find("Start"));
+        }
+    }
+
+    void PlaceCamera(Transform left, Transform right)
+    {
+        transform.position = Vector3.SmoothDamp(transform.position,
+                                                new Vector3((right.position.x + left.position.x) / 2, transform.position.y, transform.position.z),
+                                                ref velocity, 
+                                                smoothTime);
+    }
+
+    int GetCameraIndex()
+    {
+        for (int i = 0; i < stages.Count - 1; i++)
+        {
+            if (player.position.x > stages[i].transform.Find("Start").position.x && player.position.x < stages[i + 1].transform.Find("Start").position.x)
+            {
+                return i;
+            }
+        }
+
+        if (player.position.x <= stages[0].transform.Find("Start").position.x)
+        {
+            return 0;
+        }
+
+        return -1;
+    }
+}
---
+++ DancingRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown("space") && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * 400);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Collider collider = GetComponent<Collider>();
+        float rayCastSize = 0.1f;
+
+        return Physics.Raycast(
+            collider.bounds.center,
+            Vector3.down,
+            out RaycastHit rayHit,
+            collider.bounds.extents.y + rayCastSize);
+    }
+
+}
