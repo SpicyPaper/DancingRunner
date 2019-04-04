8e8c6619717a346c82057b8f080e60c1ddec9191
Kurokabe <farid.abdalla.13@gmail.com>
2019-04-04 20:22:59 +0200

    [wip] trying something with a rigidbody

---
+++ DancingRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 7f;
+    public float JumpHeight = 3f;
+    public float GroundDistance = 0.2f;
+    public LayerMask Ground;
+
+    private Rigidbody body;
+    private Vector3 inputs = Vector3.zero;
+    private bool isGrounded = true;
+
+    [SerializeField]
+    private Transform groundChecker;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+
+        inputs = Vector3.zero;
+        inputs.x = Input.GetAxis("Horizontal");
+        inputs.z = Input.GetAxis("Vertical");
+        if (inputs != Vector3.zero)
+            transform.forward = inputs;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        body.MovePosition(body.position + inputs * Speed * Time.fixedDeltaTime);
+        // Get the velocity
+        Vector3 horizontalMove = inputs * Speed;
+        
+
+        // Don't use the vertical velocity
+        horizontalMove.y = 0;
+        // Calculate the approximate distance that will be traversed
+        float distance = horizontalMove.magnitude * Time.fixedDeltaTime;
+        // Normalize horizontalMove since it should be used to indicate direction
+        horizontalMove.Normalize();
+        RaycastHit hit;
+
+        // Check if the body's current velocity will result in a collision
+        if (body.SweepTest(horizontalMove, out hit, distance))
+        {
+            Debug.Log("HITTING WALL");
+            // If so, stop the movement
+            inputs -= horizontalMove;
+        }
+        Debug.Log(inputs);
+    }
+}
