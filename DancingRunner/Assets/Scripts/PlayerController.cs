cb3b5ab86ca38f2da19a8e11335ff49e28b064cb
Farid Abdalla <farid.abdalla.13@gmail.com>
2019-04-07 15:35:41 +0200

    Simple player physics

--- DancingRunner/Assets/Scripts/PlayerController.cs
+++ DancingRunner/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,13 @@ public class PlayerController : MonoBehaviour
     public float JumpHeight = 3f;
     public float GroundDistance = 0.2f;
     public LayerMask Ground;
+    public Transform PlayerCenter;
 
     private Rigidbody body;
     private Vector3 inputs = Vector3.zero;
     private bool isGrounded = true;
+    private Vector3 movement;
+    private Vector3 sumNormals;
 
     [SerializeField]
     private Transform groundChecker;
@@ -40,26 +43,44 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        body.MovePosition(body.position + inputs * Speed * Time.fixedDeltaTime);
-        // Get the velocity
-        Vector3 horizontalMove = inputs * Speed;
+        movement = inputs * Speed * Time.fixedDeltaTime;
+
+        //// Get the velocity
+        //Vector3 horizontalMove = movement;
+
+
+        //// Don't use the vertical velocity
+        //horizontalMove.y = 0;
+        //// Calculate the approximate distance that will be traversed
+        //float distance = horizontalMove.magnitude;
+        //// Normalize horizontalMove since it should be used to indicate direction
+        //horizontalMove.Normalize();
+        //RaycastHit hit;
+        //// Check if the body's current velocity will result in a collision
+        ////if (body.SweepTest(horizontalMove, out hit, distance))
+        //RaycastHit[] hits = Physics.RaycastAll(PlayerCenter.position, horizontalMove, distance);
+        //if (hits.Length > 0)
+        //{
+
+        //    movement -= Vector3.Project(movement, -hits[0].normal);
+
+        //    // If so, stop the movement
+        //    //inputs += -hits[0].normal * Speed;
+        //}
         
+        //Vector3 cancelForce = Vector3.Project(movement, -sumNormals);
+        //if (cancelForce != movement)
+        //    movement -= Vector3.Project(movement, -sumNormals);
 
-        // Don't use the vertical velocity
-        horizontalMove.y = 0;
-        // Calculate the approximate distance that will be traversed
-        float distance = horizontalMove.magnitude * Time.fixedDeltaTime;
-        // Normalize horizontalMove since it should be used to indicate direction
-        horizontalMove.Normalize();
-        RaycastHit hit;
-
-        // Check if the body's current velocity will result in a collision
-        if (body.SweepTest(horizontalMove, out hit, distance))
+        body.MovePosition(body.position + movement);
+    }
+    
+    private void OnCollisionStay(Collision collision)
+    {
+        sumNormals = new Vector3();
+        foreach (ContactPoint contactPoint in collision.contacts)
         {
-            Debug.Log("HITTING WALL");
-            // If so, stop the movement
-            inputs -= horizontalMove;
+            sumNormals += contactPoint.normal;
         }
-        Debug.Log(inputs);
     }
 }
