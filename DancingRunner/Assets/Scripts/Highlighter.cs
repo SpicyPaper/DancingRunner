a452416a13551bb713651bcf1b34dcd10959a9f8
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-04-11 15:40:48 +0200

    Create highlither script manage it's logic

---
+++ DancingRunner/Assets/Scripts/Highlighter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlighter : MonoBehaviour
+{
+    private float speed;
+    private int startHitCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 20;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Start")
+        {
+            if(startHitCounter > 0)
+            {
+                Destroy(gameObject);
+            }
+            startHitCounter++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+    }
+}
