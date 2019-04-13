2f67a658c58c8de01b2362e62fadb4bfe739cd2f
Farid Abdalla <farid.abdalla.13@gmail.com>
2019-04-13 10:55:12 +0200

    Take into account the camera rotation when moving the player

--- DancingRunner/Assets/Scripts/Player/Character.cs
+++ DancingRunner/Assets/Scripts/Player/Character.cs
@@ -44,6 +44,8 @@ public class Character : MonoBehaviour
         }
 
         Vector3 move = new Vector3(Input.GetAxis("HorizontalP" + PlayerId), 0, Input.GetAxis("VerticalP" + PlayerId)).normalized;
+
+        move = Quaternion.LookRotation(Camera.main.transform.forward) * move;
         controller.Move(move * Time.deltaTime * Speed);
         
         if (move != Vector3.zero)
